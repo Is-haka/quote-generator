579885ca2ae3677f91882c2d6ec450280afbd90f
Is-haka <isihakamabo5@gmail.com>
2023-06-03 16:45:19 +0300

    finishing the proect for 03.06.2023

---
+++ script.js
@@ -0,0 +1,7 @@
+    var arr = [
+        {numbers: {one: 1, two: 2, three: 3, four: 4, five: 5}},
+        {alphabet: {A: "A", B: "B", C: "C", D: "D", E: "E"}}, 
+         {strings: {'1st': "Hello",'2nd': "World", '3rd': "Welcome", '4th': "To", }}
+        ];
+    // document.write(arr);
+    console.log(arr);
